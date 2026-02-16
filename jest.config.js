c55ed34e74a9d7e2d61559237ed74af4b8c96dbe
wanchai7 <wanchai.view47@gmail.com>
2026-02-16 15:31:45 +0700

    coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 export default {
-    transform: {
-      "^.+\\.jsx?$": "babel-jest"
-    }
-  };
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  // เพิ่มส่วนนี้เพื่อเปิดใช้งาน Coverage Report
+  collectCoverage: true,
+  coverageReporters: ["html", "text", "lcov"],
+  coverageDirectory: "coverage"
+};
